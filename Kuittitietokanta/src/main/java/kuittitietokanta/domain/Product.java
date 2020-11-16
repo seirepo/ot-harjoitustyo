b40c2a2fc44c0b987c35e354b5cb4756eddcbac9
Susanna <susannae.repo@gmail.com>
2020-11-16 12:59:49 +0200

    add a class for a product

---
+++ Kuittitietokanta/src/main/java/kuittitietokanta/domain/Product.java
@@ -0,0 +1,16 @@
+package kuittitietokanta.domain;
+
+/**
+ * Luokka tuotteelle
+ * @author resure
+ */
+public class Product {
+    private String name;
+    private int price;
+    private double quantity;
+    private String unit;
+    
+    public Product(String name, double price, double qty, String unit) {
+
+    }
+}
