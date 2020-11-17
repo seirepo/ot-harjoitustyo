110a939254eb58d77d6ca55c72a858e010b30fd6
Susanna <susannae.repo@gmail.com>
2020-11-17 16:26:49 +0200

    add empty test file for Receipt

---
+++ Kuittitietokanta/src/test/java/kuittitietokanta/domain/receiptTest.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kuittitietokanta.domain;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author resure
+ */
+public class receiptTest {
+    
+    @Before
+    public void setUp() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+}
