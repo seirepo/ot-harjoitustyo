8792a452dd0014fd6d4ca0d4ed0523657aaf2014
Susanna <susannae.repo@gmail.com>
2020-11-17 18:01:41 +0200

    discard attributes quantity, unit and amount leaving only name and price

--- Kuittitietokanta/src/main/java/kuittitietokanta/domain/Product.java
+++ Kuittitietokanta/src/main/java/kuittitietokanta/domain/Product.java
@@ -7,27 +7,20 @@ package kuittitietokanta.domain;
 public class Product {
     private String name;
     private int price;
-    private double quantity;
-    private String unit;
     
-    public Product(String name, double price, double qty, String unit) {
+    
+    public Product(String name, double price) {
         this.name = name;
         this.price = (int) (price * 100);
-        this.quantity = qty;
-        this.unit = unit;
     }
-    
+       
     public int getPrice() {
         return this.price;
     }
     
-    public double getQuantity() {
-        return this.quantity;
-    }
-    
-    @Override
+        @Override
     public String toString() {
         double p = this.price / 100 + this.price % 100;
-        return String.format("%s\n%2.2f\n%.2f %s\n", this.name, p, this.quantity, this.unit);
+        return String.format("%s\n%2.2f\n", this.name, p);
     }
 }
