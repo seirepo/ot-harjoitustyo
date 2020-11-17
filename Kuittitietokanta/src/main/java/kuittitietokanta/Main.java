359c80f89327bda107fe4724f9cfc617ba8366bc
Susanna <susannae.repo@gmail.com>
2020-11-17 19:53:55 +0200

    main for random testing

--- Kuittitietokanta/src/main/java/kuittitietokanta/Main.java
+++ Kuittitietokanta/src/main/java/kuittitietokanta/Main.java
@@ -1,7 +1,7 @@
 package kuittitietokanta;
 
 import kuittitietokanta.domain.*;
-import java.util.ArrayList;
+import java.util.HashMap;
 /**
  *
  * @author resure
@@ -12,15 +12,8 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Product p1 = new Product("nuudeli", 0.89, 5, "pc");
-        Product p2 = new Product("kaali", 1.35, 1.5, "kg");
-        Product p3 = new Product("tofu", 1.95, 1, "pc");
-        ArrayList<Product> products = new ArrayList<Product>();
-        products.add(p1);
-        products.add(p2);
-        products.add(p3);
-        
-        Receipt r = new Receipt("S-market", java.time.LocalDate.now(), products);
+        ReceiptItem item = new ReceiptItem("tofu", 1.95, 1, "pc");
+        System.out.println("hinta: " + item.getPrice());
     }
     
 }
