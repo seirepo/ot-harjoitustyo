08e830ef7a7b57a810b2856c508c72ffd1fb50d6
Susanna <susannae.repo@gmail.com>
2020-11-24 11:59:05 +0200

    make UNITS attribute a list, modify constructor: check for negative price and for  unknown unit

--- Kuittitietokanta/src/main/java/receiptapp/domain/ReceiptItem.java
+++ Kuittitietokanta/src/main/java/receiptapp/domain/ReceiptItem.java
@@ -1,5 +1,8 @@
 package receiptapp.domain;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 /**
  * Kuitti yhdelle kuitilla olevalle riville
  * @author resure
@@ -8,14 +11,22 @@ public class ReceiptItem {
     private String product;
     private int price; // kokonaishinta: tästä lasketaan yksikköhinta
     private int quantity;
-    private final String[] UNITS = {"pc", "kg", "l"};
+    private final ArrayList<String> UNITS = new ArrayList<>(Arrays.asList("pc", "kg", "l"));
     private String unit;
     
     public ReceiptItem(String product, double price, int quantity, String unit) {
         this.product = product;
-        this.price = (int) (price * 100);
+        if (price < 0) {
+            this.price = 0;
+        } else {
+            this.price = (int) (price * 100);
+        }
         this.quantity = quantity;
-        this.unit = unit;
+        if (this.UNITS.contains(unit)) {
+            this.unit = unit;
+        } else {
+            this.unit = "pc";
+        }
     }
     
     public int getPrice() {
@@ -36,7 +47,6 @@ public class ReceiptItem {
     
     @Override
     public String toString() {
-        //double priceInEuros = this.price / 100 + this.price % 100;
         String s = String.format("%-12s\t%-3.2f\t%-3d\t%-2s\t%-2.2fe / %-2s",
                 this.product, HelperFunctions.centsToEuros(this.price), this.quantity, this.unit,
                 HelperFunctions.centsToEuros(getUnitPrice()), this.unit);
