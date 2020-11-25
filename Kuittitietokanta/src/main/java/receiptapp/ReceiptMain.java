08f71ef1df0540735ad21644954f5bfeeea27eee
Susanna <susannae.repo@gmail.com>
2020-11-25 15:15:07 +0200

    tests with main, there might be some problems in here (suddenly exec-maven-plugin needed and classNotFoundException on receiptapp.Main)

--- Kuittitietokanta/src/main/java/receiptapp/ReceiptMain.java
+++ Kuittitietokanta/src/main/java/receiptapp/ReceiptMain.java
@@ -1,19 +1,56 @@
 package receiptapp;
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
 import receiptapp.domain.ReceiptItem;
+import receiptapp.domain.ReceiptService;
 /**
  *
  * @author resure
  */
-public class Main {
+public class ReceiptMain extends Application {
 
+//    private Stage stage;
+//    private ReceiptService receiptService;
+//    private Scene scene;
+    
+    @Override
+    public void start(Stage primaryStage) {
+        
+        try {
+            FXMLLoader ldr = new FXMLLoader(getClass().getResource("/fxml/fxReceipt.fxml"));
+            Pane root = (Pane)ldr.load();
+            ReceiptController receiptCntrl = (ReceiptController)ldr.getController();
+            Scene scene = new Scene(root);
+            
+            scene.getStylesheets().add(getClass().getResource("fxreceipt.css").toExternalForm());
+            primaryStage.setScene(scene);
+            primaryStage.setTitle("very cool");
+            
+            
+            primaryStage.show();
+            
+        } catch(Exception e) {
+            e.printStackTrace();
+            System.err.println("ReceiptMain: " + e.getMessage());
+        }
+    }
+    
+    
+    
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        ReceiptItem item1 = new ReceiptItem("tofu", 1.95, 1, "pc");
-        System.out.println("hinta: " + item1.getPrice());
+        launch(args);
         
+//        ReceiptItem item1 = new ReceiptItem("tofu", 1.95, 1, "pc");
+//        System.out.println("hinta: " + item1.getPrice());
+//        System.out.println(item1.getItem());
+
     }
     
 }
