cb5713ced16d70ac3927d38e090f7bf2d430af1f
Susanna <susannae.repo@gmail.com>
2020-11-25 15:13:37 +0200

    begin to edit controller

--- Kuittitietokanta/src/main/java/receiptapp/ReceiptController.java
+++ Kuittitietokanta/src/main/java/receiptapp/ReceiptController.java
@@ -3,17 +3,23 @@ package receiptapp;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.Initializable;
+import receiptapp.domain.ReceiptService;
 
 /**
  * FXML Controller class
  *
  * @author resure
  */
-public class FxReceiptController implements Initializable {
+public class ReceiptController implements Initializable {
 
-    /**
-     * Initializes the controller class.
-     */
+    private ReceiptService receiptService;
+    // private ReceiptMain application;
+    
+    public void setApplication(ReceiptMain application) {
+        //
+    }
+    
+    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
