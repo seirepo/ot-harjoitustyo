dedd5759d04cfef922c6938dfdd1e5a870126513
Susanna <susannae.repo@gmail.com>
2020-11-27 12:39:11 +0200

    edit controller, add fx:ids to elements and create stub methods for buttons, initialize choicebox choices

--- Kuittitietokanta/src/main/java/receiptapp/ReceiptController.java
+++ Kuittitietokanta/src/main/java/receiptapp/ReceiptController.java
@@ -2,8 +2,15 @@ package receiptapp;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
 import receiptapp.domain.ReceiptService;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 /**
  * FXML Controller class
@@ -14,6 +21,19 @@ public class ReceiptController implements Initializable {
 
     private ReceiptService receiptService;
     private ReceiptMain application;
+    
+    @FXML private TextField storeField;
+    @FXML private DatePicker date;
+    @FXML private Button cancelBtn;
+    @FXML private Button okBtn;
+    @FXML private TextField productField;
+    @FXML private TextField priceField;
+    @FXML private TextField qntyField;
+    @FXML private ChoiceBox<String> unitChoice;
+    @FXML private Button addProductBtn;
+    @FXML private Label receiptTotal;
+    @FXML private Button addReceiptBtn;
+    @FXML private Button removeReceipt;
     
     public ReceiptController() {
         
@@ -28,7 +48,34 @@ public class ReceiptController implements Initializable {
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
+        this.unitChoice.getItems().add("pc");
+        this.unitChoice.getItems().add("kg");
+        this.unitChoice.getItems().add("l");
+    }
+
+    @FXML
+    void handleAddProduct(ActionEvent event) {
+        System.out.println("klikkasit add!");
+    }
+
+    @FXML
+    void handleAddReceipt(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleCancel(ActionEvent event) {
+        System.out.println("klikkasit cancel!");
+    }
+
+    @FXML
+    void handleOk(ActionEvent event) {
+        System.out.println("klikkasit ok!");
+    }
+
+    @FXML
+    void handleRemoveReceipt(ActionEvent event) {
+
+    }
     
 }
