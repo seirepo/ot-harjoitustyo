9810d6261a88afa658f8f949900b803edf20d0db
Susanna <susannae.repo@gmail.com>
2020-11-30 18:00:36 +0200

    add method for adding a new receipt + add todos

--- Kuittitietokanta/src/main/java/receiptapp/domain/ReceiptService.java
+++ Kuittitietokanta/src/main/java/receiptapp/domain/ReceiptService.java
@@ -12,7 +12,7 @@ import java.util.ArrayList;
  * @author resure
  */
 public class ReceiptService {
-    private ArrayList<ReceiptItem> items;
+    private ArrayList<ReceiptItem> items; // voiskohan tää olla vaan controllerissa kun sitä ei täällä taida tarvita?
     private ArrayList<Receipt> receipts;
     
     
@@ -22,26 +22,40 @@ public class ReceiptService {
         this.receipts = new ArrayList<>();
     }
     
-        
+    // TODO: testit
+    public boolean addReceipt(Receipt receipt) {
+        this.receipts.add(receipt);
+        this.items.clear();
+        return true;
+    }
+    
+    // TODO: testit
     public boolean addReceiptItem(ReceiptItem item) {
         this.items.add(item);
         return true;
     }
     
+    public ArrayList<Receipt> getReceipts() {
+        return this.receipts;
+    }
+    
     public ArrayList<ReceiptItem> getReceiptItems() {
         return this.items;
     }
-    
+
+    // TODO: testit    
     public int getMeanTotal(LocalDate start, LocalDate end) {
         // hae daosta kuittien määrä annetulla välillä
         return 0;
     }
     
+    // TODO: testit    
     public ArrayList<String> getMostBoughtProducts(LocalDate start, LocalDate end, int top) {
         // hae daosta ostetuimmat tuotteet ja palauta top kappaletta tuotteita
         return new ArrayList<String>();
     }
-    
+
+    // TODO: testit    
     public String getMostVisitedStores(LocalDate start, LocalDate end, int top) {
         // hae daosta kaupat joissa on käyty eniten ja palauta niistä top kappaletta
         return "";
