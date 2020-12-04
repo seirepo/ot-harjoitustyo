e3c06385824937867a549d55f4184f2ca59b8b7c
Susanna <susannae.repo@gmail.com>
2020-12-04 18:28:28 +0200

    turn arrayList into observableArrayList

--- Kuittitietokanta/src/test/java/receiptapp/domain/ReceiptTest.java
+++ Kuittitietokanta/src/test/java/receiptapp/domain/ReceiptTest.java
@@ -1,13 +1,11 @@
 package receiptapp.domain;
 
-import org.junit.After;
-import org.junit.AfterClass;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 import java.time.LocalDate;
-import java.util.ArrayList;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 
 /**
  *
@@ -16,16 +14,16 @@ import java.util.ArrayList;
 public class ReceiptTest {
     
     Receipt receipt;
-    ArrayList<ReceiptItem> products;
+    ObservableList<ReceiptItem> products;
     ReceiptItem item1, item2, item3;
     
     @Before
     public void setUp() {
         LocalDate date = LocalDate.of(2020, 10, 10);
-        item1 = new ReceiptItem("name", 6, 4, "pc");
-        item2 = new ReceiptItem("name", 1.37, 200, "kg");
-        item3 = new ReceiptItem("name", 1.45, 1, "l");        
-        products = new ArrayList();
+        item1 = new ReceiptItem("name", 6, true, 4, "pc");
+        item2 = new ReceiptItem("name", 1.37, true, 200, "kg");
+        item3 = new ReceiptItem("name", 1.45, true, 1, "l");        
+        products = FXCollections.observableArrayList();
         receipt = new Receipt("store", date, products);
     }
 
