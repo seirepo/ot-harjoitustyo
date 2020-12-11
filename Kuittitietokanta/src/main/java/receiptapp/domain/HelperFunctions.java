3d0df00651dc494a3d6ff9b8bff19149616c2273
Susanna <susannae.repo@gmail.com>
2020-12-11 15:03:59 +0200

    replace unit conversion functions with one decimal shifting function

--- Kuittitietokanta/src/main/java/receiptapp/domain/HelperFunctions.java
+++ Kuittitietokanta/src/main/java/receiptapp/domain/HelperFunctions.java
@@ -6,31 +6,43 @@ package receiptapp.domain;
  */
 public class HelperFunctions {
         
-    /**
-     * Muuntaa annetut sentit euroiksi kahden desimaalin tarkkuudelle.
-     * @param cents sentit
-     * @return eurot doublena
-     */
-    public static double centsToEuros(int cents) {
-        return cents / 100 + ((cents % 100) * 1.0 / 100);
-    }
+//    /**
+//     * Muuntaa annetut sentit euroiksi kahden desimaalin tarkkuudelle.
+//     * @param cents sentit
+//     * @return eurot doublena
+//     */
+//    public static double centsToEuros(int cents) {
+//        return cents / 100 + ((cents % 100) * 1.0 / 100);
+//    }
+//    
+//    /**
+//     * Muuntaa annetut grammat kilogrammoiksi kolmen desimaalin tarkkuudelle.
+//     * @param grams grammat
+//     * @return kilogrammat doublena
+//     */
+//    public static double gToKg(int grams) {
+//        return (grams / 1000) + ((grams % 1000) * 1.0 / 1000);
+//    }
+//    
+//    /**
+//     * Muuntaa annetut millilitrat litroiksi kolmen desimaalin tarkkuudelle
+//     * käyttäen metodia gToKg().
+//     * @param ml millilitrat
+//     * @return litrat doublena
+//     */
+//    public static double mlToL(int ml) {
+//        return gToKg(ml);
+//    }
     
     /**
-     * Muuntaa annetut grammat kilogrammoiksi kolmen desimaalin tarkkuudelle.
-     * @param grams grammat
-     * @return kilogrammat doublena
+     * Funktio joka mahdollistaa yksikkömuunnokset liu'uttamalla doublea.
+     * Lukua liikutetaan desimaalipisteen suhteen joko oikealle antamalla
+     * negatiivisen siirron ja muutoin vasemmalle.
+     * @param value liu'utettava luku
+     * @param shift siirron määrä
+     * @return 
      */
-    public static double gToKg(int grams) {
-        return (grams / 1000) + ((grams % 1000) * 1.0 / 1000);
-    }
-    
-    /**
-     * Muuntaa annetut millilitrat litroiksi kolmen desimaalin tarkkuudelle
-     * käyttäen metodia gToKg().
-     * @param ml millilitrat
-     * @return litrat doublena
-     */
-    public static double mlToL(int ml) {
-        return gToKg(ml);
+    public static double shiftDouble(double value, int shift) {
+        return value * Math.pow(10, shift);
     }
 }
--- Kuittitietokanta/src/test/java/receiptapp/domain/HelperFunctionsTest.java
+++ Kuittitietokanta/src/test/java/receiptapp/domain/HelperFunctionsTest.java
@@ -22,28 +22,46 @@ public class HelperFunctionsTest {
     public void setUp() {
     }
     
+//    @Test
+//    public void centsToEurosReturnsRightWhenCentsLessThan100() {
+//        assertEquals(0.75, HelperFunctions.centsToEuros(75), 0.001);
+//    }
+//    
+//    @Test
+//    public void centsToEurosReturnsRightWhenCentsMoreThan100() {
+//        assertEquals(99.05, HelperFunctions.centsToEuros(9905), 0.001);
+//    }
+//    
+//    @Test
+//    public void centsToEurosReturnsRightWhenCentsLessThan0() {
+//        assertEquals(-3.8, HelperFunctions.centsToEuros(-380), 0.001);
+//    }
+//    
+//    @Test
+//    public void gToKgRightWhenGLessThan1000() {
+//        assertEquals(0.321, HelperFunctions.gToKg(321), 0.001);
+//    }
+//    
+//    @Test
+//    public void gToKgRightWhenGMoreThan1000() {
+//        assertEquals(1.321, HelperFunctions.gToKg(1321), 0.001);
+//    }
+    
     @Test
-    public void centsToEurosReturnsRightWhenCentsLessThan100() {
-        assertEquals(0.75, HelperFunctions.centsToEuros(75), 0.001);
+    public void shiftDoubletShiftsDoubleToLeft() {
+        assertEquals(1321000, HelperFunctions.shiftDouble(1321, 3), 0.001);
+        assertEquals(-2300000, HelperFunctions.shiftDouble(-23, 5), 0.001);
+        assertEquals(1234.5678, HelperFunctions.shiftDouble(0.12345678, 4), 0.001);
+        assertEquals(22.4567, HelperFunctions.shiftDouble(2.24567, 1), 0.001);
+        assertEquals(1.5, HelperFunctions.shiftDouble(1.5, 0), 0.001);
     }
     
     @Test
-    public void centsToEurosReturnsRightWhenCentsMoreThan100() {
-        assertEquals(99.05, HelperFunctions.centsToEuros(9905), 0.001);
+    public void shiftDoubleShiftsDoubleToRight() {
+        assertEquals(1.321, HelperFunctions.shiftDouble(1321, -3), 0.001);
+        assertEquals(-0.00023, HelperFunctions.shiftDouble(-23, -5), 0.001);
+        assertEquals(99.05, HelperFunctions.shiftDouble(9905, -2), 0.001);
+        assertEquals(123.456, HelperFunctions.shiftDouble(12345.6, -2), 0.01);
+        assertEquals(1.5, HelperFunctions.shiftDouble(1.5, 0), 0.001);
     }
-    
-    @Test
-    public void centsToEurosReturnsRightWhenCentsLessThan0() {
-        assertEquals(-3.8, HelperFunctions.centsToEuros(-380), 0.001);
-    }
-    
-    @Test
-    public void gToKgRightWhenGLessThan1000() {
-        assertEquals(0.321, HelperFunctions.gToKg(321), 0.001);
-    }
-    
-    @Test
-    public void gToKgRightWhenGMoreThan1000() {
-        assertEquals(1.321, HelperFunctions.gToKg(1321), 0.001);
-    }    
 }
